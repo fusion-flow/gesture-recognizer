8445be0b209dc05e5128896fa1d8411154c63825
AwesomeNipun <nipun1969@gmail.com>
2023-12-20 14:39:27 +0530

    Create the api call

--- app.py
+++ app.py
@@ -11,50 +11,49 @@ import mediapipe as mp
 from model import KeyPointClassifier
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--width", help="cap width", type=int, default=960)
-    parser.add_argument("--height", help="cap height", type=int, default=540)
-
-    parser.add_argument("--use_static_image_mode", action="store_true")
-    parser.add_argument(
-        "--min_detection_confidence",
-        help="min_detection_confidence",
-        type=float,
-        default=0.7,
-    )
-    parser.add_argument(
-        "--min_tracking_confidence",
-        help="min_tracking_confidence",
-        type=int,
-        default=0.5,
-    )
-
-    args = parser.parse_args()
+def get_args(
+    device=0,
+    width=960,
+    height=540,
+    use_static_image_mode=False,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.5,
+):
+    args = {
+        "device": device,
+        "width": width,
+        "height": height,
+        "use_static_image_mode": use_static_image_mode,
+        "min_detection_confidence": min_detection_confidence,
+        "min_tracking_confidence": min_tracking_confidence,
+    }
 
     return args
+    # def main_():
 
-
-def main():
-    # Argument parsing #################################################################
-    args = get_args()
-
-    use_static_image_mode = args.use_static_image_mode
-    min_detection_confidence = args.min_detection_confidence
-    min_tracking_confidence = args.min_tracking_confidence
-
+    min_detection_confidence = 0.7
+    min_tracking_confidence = 0.5
     # use image as input
+    img = cv.imread("sample images/123.jpg")
 
     # Model load #############################################################
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
-        static_image_mode=use_static_image_mode,
         max_num_hands=2,
         min_detection_confidence=min_detection_confidence,
         min_tracking_confidence=min_tracking_confidence,
     )
+
+    if img is None:
+        raise ValueError("Image is not given")
+
+    # Make sure the image is a valid numpy array
+    if not isinstance(img, np.ndarray):
+        raise ValueError("Invalid input type")
+
+    # Ensure the image has three channels (BGR)
+    if img.shape[2] != 3:
+        raise ValueError("Invalid number of channels")
 
     keypoint_classifier = KeyPointClassifier()
 
@@ -66,7 +65,7 @@ def main():
         keypoint_classifier_labels = [row[0] for row in keypoint_classifier_labels]
 
     # image acquisition #####################################################
-    image = cv.imread("sample images/sample_6.jpg")
+    image = img
 
     image = cv.flip(image, 1)  # Mirror display
     debug_image = copy.deepcopy(image)
@@ -94,7 +93,6 @@ def main():
 
             final_val += int(keypoint_classifier_labels[hand_sign_id])
 
-    print("You are showing: ", final_val)
     # if both hands are availale take average of both confidence scores
     score = 0
     if (
@@ -105,7 +103,85 @@ def main():
             results.multi_handedness[1].classification[0].score
             + results.multi_handedness[0].classification[0].score
         ) / 2
-    print("Confidence score: ", score)
+    # return with 4 decimal places
+    return final_val, round(score, 4)
+
+
+def main(img, min_detection_confidence=0.7, min_tracking_confidence=0.5):
+    # use image as input
+    img = cv.imread("sample images/123.jpg")
+
+    # Model load #############################################################
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(
+        max_num_hands=2,
+        min_detection_confidence=min_detection_confidence,
+        min_tracking_confidence=min_tracking_confidence,
+    )
+
+    if img is None:
+        raise ValueError("Image is not given")
+
+    # Make sure the image is a valid numpy array
+    if not isinstance(img, np.ndarray):
+        raise ValueError("Invalid input type")
+
+    # Ensure the image has three channels (BGR)
+    if img.shape[2] != 3:
+        raise ValueError("Invalid number of channels")
+
+    keypoint_classifier = KeyPointClassifier()
+
+    # Read labels ###########################################################
+    with open(
+        "model/keypoint_classifier/keypoint_classifier_label.csv", encoding="utf-8-sig"
+    ) as f:
+        keypoint_classifier_labels = csv.reader(f)
+        keypoint_classifier_labels = [row[0] for row in keypoint_classifier_labels]
+
+    # image acquisition #####################################################
+    image = img
+
+    image = cv.flip(image, 1)  # Mirror display
+    debug_image = copy.deepcopy(image)
+
+    # Detection implementation #############################################################
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = hands.process(image)
+    image.flags.writeable = True
+
+    if results.multi_hand_landmarks is None:
+        return 0, 0
+    #  ####################################################################
+    final_val = 0
+    if results.multi_hand_landmarks is not None:
+        for hand_landmarks, handedness in zip(
+            results.multi_hand_landmarks, results.multi_handedness
+        ):
+            # Landmark calculation
+            landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+            # Conversion to relative coordinates / normalized coordinates
+            pre_processed_landmark_list = pre_process_landmark(landmark_list)
+
+            # Hand sign classification
+            hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+
+            final_val += int(keypoint_classifier_labels[hand_sign_id])
+
+    # if both hands are availale take average of both confidence scores
+    score = 0
+    if (
+        results.multi_handedness[1] is not None
+        and results.multi_handedness[0] is not None
+    ):
+        score = (
+            results.multi_handedness[1].classification[0].score
+            + results.multi_handedness[0].classification[0].score
+        ) / 2
+    # return with 4 decimal places
+    return final_val, round(score, 4)
 
 
 def calc_bounding_rect(image, landmarks):
